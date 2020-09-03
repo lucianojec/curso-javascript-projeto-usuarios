1b44b5f7fffc6fa27502b69a795a53002b4f2399
luciano.fagundes <luciano.fagundes@softplan.com.br>
2020-09-02 22:41:17 -0300

    Aula 44 curso hcode

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,104 +1,125 @@
 class UserController {
 
-    constructor(formId, tableId){
+    constructor(formId, tableId) {
 
-        this.formEl = document.getElementById(formId)        
-        this.tableEl = document.getElementById(tableId)        
+        this.formEl = document.getElementById(formId)
+        this.tableEl = document.getElementById(tableId)
 
         this.onSubmit()
-       
+
     }
 
-    onSubmit(){
-        
+    onSubmit() {
+
         this.formEl.addEventListener('submit', (event) => {
-    
+
             event.preventDefault();
 
             let values = this.getValues()
-            
-            this.getPhoto((content)=>{
 
-                values.photo = content
+            this.getPhoto().then(
+                (content) => {
 
-                this.addLine(values)
+                    values.photo = content
 
-            })            
+                    this.addLine(values)
+
+                },
+                (e) => {
+                    console.error(e)
+
+                }
+            )
 
         })
-    
-    }
-
-    getPhoto(callback){
-        
-
-        let fileReader = new FileReader();
-
-        let elements = [...this.formEl.elements].filter(item=>{
-
-             if (item.name === 'photo') {
-                 
-                return item
-            }        
-            
-        });
-
-        let file = elements[0].files[0]
-
-        fileReader.onload = ()=>{
-
-             callback(fileReader.result)
-
-        }
-
-        fileReader.readAsDataURL(file)
 
     }
 
-    getValues(){
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+
+                    return item
+                }
+
+            });
+
+            let file = elements[0].files[0]
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result)
+
+            }
+
+            fileReader.onerror = (e) => {
+                reject(e)
+            }
+
+            (file) ? fileReader.readAsDataURL(file) : resolve('dist/img/boxed-bg.jpg')
+
+        })
+
+    }
+
+    getValues() {
 
         let user = {};
 
         [...this.formEl.elements].forEach((field, index) => {
-    
-            if (field.name =='gender'){
-        
-                if (field.checked) 
+
+            if (field.name == 'gender') {
+
+                if (field.checked)
                     user[field.name] = field.value
-            }else
-                user[field.name] = field.value    
-        
+
+            } else if (field.name == 'admin') {
+
+                user[field.name] = field.checked
+
+            } else
+                user[field.name] = field.value
+
         })
-    
-        return new User( 
-            user.name, 
-            user.gender, 
-            user.birth, 
-            user.country, 
-            user.email, 
-            user.password, 
-            user.photo, 
+
+        return new User(
+            user.name,
+            user.gender,
+            user.birth,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
             user.admin
         )
 
     }
 
-    addLine(dataUser){
+    addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `           
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>           
     
-        `        
+       `;
+
+        this.tableEl.appendChild(tr)
+
     }
 
 
