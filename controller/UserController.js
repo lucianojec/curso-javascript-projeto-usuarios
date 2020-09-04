2b1d98b428b1cf155164e8dea2c89a4bd2d6b427
luciano.fagundes <luciano.fagundes@softplan.com.br>
2020-09-03 23:44:07 -0300

    aula 47

--- controller/UserController.js
+++ controller/UserController.js
@@ -6,6 +6,17 @@ class UserController {
         this.tableEl = document.getElementById(tableId)
 
         this.onSubmit()
+        this.onEdit()
+
+    }
+
+    onEdit() {
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener('click', e => {
+
+            this.showPanelCreate()
+
+        })
 
     }
 
@@ -16,7 +27,7 @@ class UserController {
             event.preventDefault();
 
             let btn = this.formEl.querySelector('[type=submit]')
-            
+
             btn.disabled = true
 
             let values = this.getValues()
@@ -85,7 +96,7 @@ class UserController {
 
         [...this.formEl.elements].forEach((field, index) => {
 
-            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
 
                 field.parentElement.classList.add('has-error')
                 isValid = false
@@ -106,7 +117,7 @@ class UserController {
 
         })
 
-        if (!isValid){
+        if (!isValid) {
             return false
         }
 
@@ -136,11 +147,19 @@ class UserController {
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>           
     
        `;
+
+        tr.querySelector('.btn-edit').addEventListener('click', e => {
+
+            console.log(JSON.parse(tr.dataset.user))
+
+            this.showPanelUpdate()
+
+        })
 
         this.tableEl.appendChild(tr)
 
@@ -148,12 +167,25 @@ class UserController {
 
     }
 
-    updateCount(){
+    showPanelCreate() {
+
+        document.querySelector('#box-user-create').style.display = "block"
+        document.querySelector('#box-user-update').style.display = "none"
+
+    }
+
+    showPanelUpdate() {
+
+        document.querySelector('#box-user-create').style.display = "none"
+        document.querySelector('#box-user-update').style.display = "block"
+
+    }
+    updateCount() {
 
         let numberUsers = 0
         let numberAdmin = 0;
 
-        [...this.tableEl.children].forEach(tr=>{
+        [...this.tableEl.children].forEach(tr => {
 
             numberUsers++
 
