23f7940a93b7f128c044a8e31c3586bd9909ae5e
luciano.fagundes <luciano.fagundes@softplan.com.br>
2020-09-11 00:08:23 -0300

    Finaliza Aula Hcode-Udemy da Seçao 3: Ger. Usuarios

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,12 +1,14 @@
 class UserController {
 
-    constructor(formId, tableId) {
+    constructor(formIdCreate, formIdUpdate, tableId) {
 
-        this.formEl = document.getElementById(formId)
+        this.formEl = document.getElementById(formIdCreate)
+        this.formUpdateEl = document.getElementById(formIdUpdate)
         this.tableEl = document.getElementById(tableId)
 
         this.onSubmit()
         this.onEdit()
+        this.selectAll()
 
     }
 
@@ -15,6 +17,63 @@ class UserController {
         document.querySelector("#box-user-update .btn-cancel").addEventListener('click', e => {
 
             this.showPanelCreate()
+
+        })
+
+        this.formUpdateEl.addEventListener('submit', event => {
+
+            event.preventDefault();
+
+            let btn = this.formUpdateEl.querySelector('[type=submit]')
+
+            btn.disabled = true
+
+            let values = this.getValues(this.formUpdateEl)
+
+            let index = this.formUpdateEl.dataset.trindex
+
+            let tr = this.tableEl.rows[index]
+
+            let userOld = JSON.parse(tr.dataset.user)
+
+            let result = Object.assign({}, userOld, values)
+
+            this.getPhoto(this.formUpdateEl).then(
+                (content) => {
+
+
+                    if (!values.photo) {
+
+                        result._photo = userOld._photo
+
+                    } else {
+                        result._photo = content
+
+                    }
+
+                    let user = new User()
+
+                    user.loadFromJSON(result)
+
+                    this.getTr(user, tr)
+
+                    user.save()
+
+                    this.addEventTr(tr)
+
+                    this.updateCount()
+
+                    this.formUpdateEl.reset()
+                    
+                    btn.disabled = false
+                    
+                    this.showPanelCreate()
+                },
+                (e) => {
+                    console.error(e)
+
+                }
+            )
 
         })
 
@@ -30,14 +89,16 @@ class UserController {
 
             btn.disabled = true
 
-            let values = this.getValues()
+            let values = this.getValues(this.formEl)
 
             if (!values) return false
 
-            this.getPhoto().then(
+            this.getPhoto(this.formEl).then(
                 (content) => {
 
                     values.photo = content
+
+                    values.save()
 
                     this.addLine(values)
 
@@ -56,13 +117,13 @@ class UserController {
 
     }
 
-    getPhoto() {
+    getPhoto(formEl) {
 
         return new Promise((resolve, reject) => {
 
             let fileReader = new FileReader();
 
-            let elements = [...this.formEl.elements].filter(item => {
+            let elements = [...formEl.elements].filter(item => {
 
                 if (item.name === 'photo') {
 
@@ -89,12 +150,12 @@ class UserController {
 
     }
 
-    getValues() {
+    getValues(formEl) {
 
         let user = {};
         let isValid = true;
 
-        [...this.formEl.elements].forEach((field, index) => {
+        [...formEl.elements].forEach((field, index) => {
 
             if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
 
@@ -134,9 +195,36 @@ class UserController {
 
     }
 
+    selectAll(){
+
+        let users = User.getUsersStorage()
+
+        users.forEach(dataUser=>{
+
+            let user = new User()
+
+            user.loadFromJSON(dataUser)
+
+            this.addLine(user)
+
+        })
+
+    }
+
     addLine(dataUser) {
 
-        let tr = document.createElement('tr');
+        let tr = this.getTr(dataUser)
+        
+        this.tableEl.appendChild(tr)
+
+        this.updateCount()
+
+    }
+
+    getTr(dataUser, tr = null){
+
+        if (tr === null) tr = document.createElement('tr');
+
 
         tr.dataset.user = JSON.stringify(dataUser)
 
@@ -148,22 +236,77 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>           
     
        `;
 
+       this.addEventTr(tr)
+
+       return tr
+
+    }
+
+    addEventTr(tr) {
+
+        tr.querySelector('.btn-delete').addEventListener('click', e => {
+
+            if (confirm("Deseja realmente excluir?")){
+
+                let user = new User()
+
+                user.loadFromJSON(JSON.parse(tr.dataset.user))
+
+                user.remove()
+
+
+                tr.remove()
+
+                this.updateCount()
+
+            }
+
+        })
+
         tr.querySelector('.btn-edit').addEventListener('click', e => {
 
-            console.log(JSON.parse(tr.dataset.user))
+            let json = JSON.parse(tr.dataset.user)
+
+            this.formUpdateEl.dataset.trindex = tr.sectionRowIndex;
+
+            for (let name in json) {
+
+                let field = this.formUpdateEl.querySelector("[name=" + name.replace("_", "") + "]")
+
+                if (field) {
+
+
+                    switch (field.type) {
+                        case 'file':
+                            continue
+                            break
+
+                        case 'radio':
+                            field = this.formUpdateEl.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]")
+                            field.checked = true
+                            break
+                        case 'checkbox':
+                            field.checked = json[name]
+                            break
+                        default:
+                            field.value = json[name]
+
+                    }
+
+                }
+
+            }
+
+            this.formUpdateEl.querySelector(".photo").src = json._photo
 
             this.showPanelUpdate()
 
         })
-
-        this.tableEl.appendChild(tr)
-
-        this.updateCount()
 
     }
 
--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ class User {
 
     constructor(name, gender, birth, country, email, password, photo, admin) {
 
+        this._id;
         this._name = name
         this._gender = gender
         this._birth = birth
@@ -14,7 +15,11 @@ class User {
 
     }
 
-    get register(){
+    get id() {
+        return this._id
+    }
+
+    get register() {
         return this._register
     }
 
@@ -50,10 +55,103 @@ class User {
         return this._admin
     }
 
-    set photo(value){
+    set photo(value) {
         this._photo = value
     }
 
+    loadFromJSON(json) {
 
+        for (let name in json) {
+
+            switch (name) {
+
+                case '_register':
+                    this[name] = new Date(json[name])
+                    break
+                default:
+                    this[name] = json[name]
+
+            }
+            
+
+        }
+
+    }
+
+    static getUsersStorage() {
+
+        let users = []
+
+        if (localStorage.getItem("users")) {
+
+            users = JSON.parse(localStorage.getItem('users'))
+
+        }
+
+        return users
+
+    }
+
+    getNewID() {
+
+        let usersID = parseInt(localStorage.getItem("usersID"))
+
+        if (!usersID > 0) usersID = 0
+
+        usersID++
+
+        localStorage.setItem("usersID", usersID)
+
+        return usersID
+
+    }
+
+    save() {
+
+        let users = User.getUsersStorage();
+
+        if (this.id > 0) {
+
+            users.map(u => {
+
+                if (u._id == this.id) {
+
+                    Object.assign(u, this)
+
+                }
+
+                return u
+
+            })
+
+        } else {
+
+            this._id = this.getNewID()
+
+            users.push(this)
+
+        }
+
+        localStorage.setItem("users", JSON.stringify(users))
+
+    }
+
+    remove() {
+
+        let users = User.getUsersStorage()
+
+        users.forEach((userData, index) => {
+
+            if (this._id == userData._id) {
+
+                users.splice(index, 1)
+
+            }
+
+        });
+
+        localStorage.setItem("users", JSON.stringify(users))
+
+    }
 
 }
